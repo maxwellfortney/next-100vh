5796f3098225ffffe112beb858808bdfda3dfdce
Jack Warner <jyw8jcu@virginia.edu>
2021-07-26 23:32:24 -0400

    added button component. Need to figure out how to change border into gradient and transition speed slower

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,12 +1,13 @@
 import Navbar from "../components/Navbar/Navbar";
+import Button from "../components/Button/Button";
 
 export default function About() {
-    return (
-        <div className="flex flex-col items-center justify-center w-full min-h-screen bg-100vh-gray">
-            <Navbar />
-            <div className="flex flex-col items-center justify-center flex-1 w-full h-full text-white">
-                a
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex flex-col items-center justify-center w-full min-h-screen bg-100vh-gray">
+      <Navbar />
+      <div className="flex flex-col items-center justify-center flex-1 w-full h-full text-white">
+        a
+      </div>
+    </div>
+  );
 }
